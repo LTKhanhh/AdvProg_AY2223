f2f500a0bedac5fa10fc59530fa21ea741bf21e1
Le Trong Khanh <124890799+LTKhanhh@users.noreply.github.com>
2023-02-26 08:36:00 +0700

    Update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,7 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    return rand()%100+1;
 }
 
 
@@ -26,8 +26,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    std::cout<<"Your guest: ";
+    int num;
+    std::cin>>num;
+    return num;
 }
 
 
@@ -40,12 +42,16 @@ int getPlayerGuess() {
 ***/
 string getAnswer(int number, int randomNumber) {
     /*** 
+    string answer;
         TODO: check number with randomNumber and return the result.
               If number is higher than randomNumber, the answer is "Your number is higher."
+              if(number>randomNumber)  answer="Your number is higher.";
+              else if(number<randomNumber) answer= "Your number is lower.";
+              else answer= "Congratulation! You win.";
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
-    string answer;
+    
 
     return answer;
 }
@@ -59,7 +65,7 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
+    if(answer!="Congratulation! You win.") return 0;
     return true;
 }
 
@@ -73,7 +79,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if(isContinued==Y) result= true;
     return result;
 }
 
@@ -87,7 +93,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    std::cout<<"DO you want to play again? (Y/N)";
+    std::cin>>isContinued;
     return isContinued;
 }
 
