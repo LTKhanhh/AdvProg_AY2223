9e7a7c134c31f24b9329c0b2ec8d95037f862f21
Le Trong Khanh <124890799+LTKhanhh@users.noreply.github.com>
2023-05-05 00:14:59 +0700

    add snake

--- AdvProg_L7-Snake/Snake.cpp
+++ AdvProg_L7-Snake/Snake.cpp
@@ -54,6 +54,8 @@ void Snake::growAtFront(Position newPosition)
     // head of snake grow at new position
 	
     /* YOUR CODE HERE */
+	 head->next = new SnakeNode( newPosition);
+    head = head->next;
 }
 
 
@@ -81,17 +83,23 @@ void Snake::growAtFront(Position newPosition)
 
 void Snake::slideTo(Position newPosition)
 {
-	if (tail->next == nullptr) { 
+	if (tail->next == nullptr) {
         // position is assigned by new position.
 		/* YOUR CODE HERE */
+		tail->position = newPosition;
 	}
 	else {
 		SnakeNode *oldTailNode = tail;
 		//cut the old tail off the snake
         /* YOUR CODE HERE */
-		
+        tail = tail->next;
+        oldTailNode->next = nullptr;
+
 		// move it to the head of the snake
         /* YOUR CODE HERE */
+        oldTailNode->position = newPosition;
+        head->next = oldTailNode;
+
 		head = oldTailNode;
 	}
 }
@@ -111,6 +119,7 @@ void Snake::slideTo(Position newPosition)
 void Snake::eatCherry()
 {
 	/* YOUR CODE HERE */
+	cherry++;
 }
 
 /*** 
@@ -144,16 +153,22 @@ void Snake::move(Direction direction)
     Position newPosition = head->position.move(direction);
 
     /* YOUR CODE HERE */
-    
-    // If gameOver, return ; 
+    game.snakeMoveTo( newPosition );
+
+    // If gameOver, return ;
     /* YOUR CODE HERE */
+    if( game.isGameOver() )
+        return;
 
     // If cherry > 0, cherry descrease one and growAtFront() with newPosition
     if (cherry > 0) {
         /* YOUR CODE HERE */
+        cherry--;
+        growAtFront( newPosition );
     } else {
     	game.snakeLeave(tail->position);
-        /* YOUR CODE HERE */        
+        /* YOUR CODE HERE */
+        slideTo( newPosition );
     }
 }
 
